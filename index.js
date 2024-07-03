d9fa0efba268cfcca1c12e435357a267797d91ed
shmam <shmam@github.com>
2024-07-03 14:42:41 -0400

    fix: using braces

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ var Client = function (host, opts) {
 
   if (!opts) opts = {}
 
-  if (typeof host === 'string') host = [host]
+  if (typeof host === 'string') {
+    host = [host]
+  }
 
   var disc = opts.token || host?.some(h => h.startsWith('http://discovery.etcd.io/'))
 
